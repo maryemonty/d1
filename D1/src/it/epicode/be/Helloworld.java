37301a3029b3bb0e0c2791ddcf2c99d6a992eae6
maryemonty <113999089+maryemonty@users.noreply.github.com>
2023-05-02 16:53:24 +0200

    Add files via upload

---
+++ D1/src/it/epicode/be/Helloworld.java
@@ -0,0 +1,39 @@
+package it.epicode.be;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Helloworld {
+
+public static void main (String[] args)
+		{
+		// Stampa a console  la frase
+		System.out.println("This is my first Epicode Java Project!");
+		
+		/*Scanner molt = new Scanner(System.in);
+		System.out.println("Inserisci il primo numero intero");
+		int num = molt.nextInt();
+		System.out.println("Inserisci un intero con cui moltiplicare il primo numero che hai scelto");
+		int num2 = molt.nextInt();
+		int risultato = num * num2;
+		System.out.println(num + " moltiplicato per " + num2 + " equivale a: " + risultato);
+		molt.close();*/
+		
+		/* Scanner conc = new Scanner(System.in);
+		System.out.println("Inserisci un testo");
+		String text = conc.nextLine();
+		System.out.println("Inserisci un intero");
+		int numConc = conc.nextInt();
+		String result = text + " " + numConc;
+		System.out.println(result);
+		conc.close(); */
+		
+		String parole[] = {"ciao", "a", "Gesù", "amen", "back-end"};
+		
+		
+		}
+
+}
+
+
+
+
